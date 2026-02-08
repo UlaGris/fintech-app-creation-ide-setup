f1023cab99cd9c50797dfceb432916e685b6f379
Ula Griškevičiūtė <ula.griskeviciute@gmail.com>
2026-02-08 18:38:27 +0200

    added button that changes text color

--- app/src/main/java/com/example/fintechappcreation_idesetup/MainActivity.kt
+++ app/src/main/java/com/example/fintechappcreation_idesetup/MainActivity.kt
@@ -1,5 +1,6 @@
 package com.example.fintechappcreation_idesetup
 
+import android.graphics.Color
 import android.os.Bundle
 import android.widget.Button
 import android.widget.TextView
@@ -15,10 +16,15 @@ class MainActivity : AppCompatActivity() {
         setContentView(R.layout.activity_main)
 
         val tvMessage = findViewById<TextView>(R.id.tvMessage)
-        val btnShowText = findViewById<Button>(R.id.btnShowText)
 
+        val btnShowText = findViewById<Button>(R.id.btnChangeText)
         btnShowText.setOnClickListener {
             tvMessage.setText(R.string.goodbye_world)
+        }
+
+        val btnChangeTextColor = findViewById<Button>(R.id.btnChangeTextColor)
+        btnChangeTextColor.setOnClickListener {
+            tvMessage.setTextColor(Color.RED)
         }
 
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
